ba34f5be24fe658a045888825144ef6d301461c0
Amar Mesic <amarmesic@gmail.com>
2014-11-16 20:27:59 +0100

    - AssemblyInfo.cs update

--- DrawerLayout/Properties/AssemblyInfo.cs
+++ DrawerLayout/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DrawerLayout")]
-[assembly: AssemblyDescription("Android like Drawer Layout for Windows Phone")]
+[assembly: AssemblyDescription("Navigation Drawer Layout for Windows Phone 8.1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amar Mesic")]
 [assembly: AssemblyProduct("DrawerLayout")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
